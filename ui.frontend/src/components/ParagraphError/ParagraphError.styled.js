cec934797d6db82818a286fa9fb3bacb69966732
Brunna Lage <brunnalage@gmail.com>
2022-07-30 19:26:44 -0300

    Style: delete old title responsive style

--- ui.frontend/src/components/ParagraphError/ParagraphError.styled.js
+++ ui.frontend/src/components/ParagraphError/ParagraphError.styled.js
@@ -3,13 +3,6 @@ import styled from 'styled-components';
 export const PContainer = styled.div`
 max-width: 16.931rem;
 margin-bottom: 3.906rem;
-@media (max-width: 768px) {
-max-width:80%;
-}
-@media (max-width: 400px) {
-max-width:100%;
-margin-bottom: 2.5rem;
-}
 `;
 
 export const ParagraphErrorStyled = styled.p`
