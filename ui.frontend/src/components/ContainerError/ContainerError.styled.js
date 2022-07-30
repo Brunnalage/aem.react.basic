e4bc2ed06efe058a91320e845e1dec2c220467fc
Brunna Lage <brunnalage@gmail.com>
2022-07-30 19:37:53 -0300

    Style: adding responsive style to ContainerError component

--- ui.frontend/src/components/ContainerError/ContainerError.styled.js
+++ ui.frontend/src/components/ContainerError/ContainerError.styled.js
@@ -4,4 +4,9 @@ export const ContainerErrorStyled = styled.div`
 display: grid;
 grid-template-columns: 50% 50%;
 grid-template-rows: auto auto auto;
+
+@media (max-width: 768px){
+    grid-template-columns: auto;
+    justify-content: center;
+}
 `;
