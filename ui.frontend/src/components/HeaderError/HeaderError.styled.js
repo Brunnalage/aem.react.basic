6c5cfaeb6a7c970cf337def418a470ea28f36538
Brunna Lage <brunnalage@gmail.com>
2022-07-29 21:09:50 -0300

    Style:  creating the HeaderError component styles and installing the styled-components library

---
+++ ui.frontend/src/components/HeaderError/HeaderError.styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+margin-bottom: 12.5rem;
+@media (max-width: 768px) {
+    margin-bottom: 4rem;
+}
+@media (max-width: 400px) {
+    margin-bottom: 3rem;
+}
+`;
+
+export const HeaderErrorStyled = styled.h3`
+font-family: 'Inconsolata', monospace !important;
+font-style: normal;
+font-weight: 700;
+font-size: 1.5rem;
+line-height: 1.563rem;
+letter-spacing: -0.08em;
+text-transform: uppercase;
+margin: 0;
+`;
